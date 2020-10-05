d4161cff0b81dbc781c5756e223cd5f005af8a91
Ricardo Galli <ricardo.galli@webbeds.com>
2020-10-05 12:49:15 +0200

    Cleaner compile

--- julia/Geoclose/compile.sh
+++ julia/Geoclose/compile.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
 
-julia -p auto -t auto -q --project=. -e 'using PackageCompiler;\
-    create_app("", "app", app_name="geoclose", force=true, filter_stdlibs=true, precompile_execution_file="precompile_app.jl")'
+julia -q --project=. -e '
+    using PackageCompiler
+    create_app("",
+        "app",
+        app_name="geoclose",
+        force=true,
+        filter_stdlibs=true,
+        precompile_execution_file="precompile_app.jl"
+    )'
